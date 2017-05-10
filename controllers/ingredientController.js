dc3ba177465e146a0fe1d17b88dd773969e46cd7
鈴木貴大 <TakahiroSuzuki@suzukikidai-no-MacBook-Air.local>
2017-05-11 09:44:53 +1000

    refactor

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -1,7 +1,6 @@
-// /controllers/ingredientController.js
-
 const mongoose = require('mongoose');
 const Ingredient = require('../models/Ingredient');
+
 
 exports.getIngredients = (req, res) => {
 	Ingredient.find()
@@ -10,5 +9,33 @@ exports.getIngredients = (req, res) => {
 				title: 'Ingredients',
 				ingredients: ingredients
 			})
+		})
+};
+
+exports.getNewingredients = (req, res) => {
+	console.log('req.body is:', req.body);
+	const name = req.body.ingredient_name;
+	let ingredient = new Ingredient();
+	ingredient.name = name;
+	ingredient.save()
+		.then(() => {
+			res.redirect('/')
+		})
+};
+
+exports.getEditingredients = (req, res) => {
+	Ingredient.findOne({ _id: req.params.id })
+		.then(ingredient => {
+			res.render('editIngredient', {ingredient: ingredient});
+		})
+};
+
+exports.postEditingredients = (req, res) => {
+	console.log('reqy.body:', req.body);
+	Ingredient.findOneAndUpdate({ _id: req.params.id }, req.body, {
+		new: true // returns new ingredient
+	})
+		.then(ingredient => {
+			res.redirect('/')
 		})
 };
--- routes/index.js
+++ routes/index.js
@@ -6,41 +6,8 @@ const ingredientController = require('../controllers/ingredientController');
 
 /* GET home page. */
 router.get('/', ingredientController.getIngredients);
-
-router.post('/', (req, res) => {
-	console.log('reqy.body is:', req.body);
-	const name = req.body.ingredient_name;
-	let ingredient = new Ingredient();
-	ingredient.name = name;
-	ingredient.save()
-		.then(() => {
-			res.redirect('/')
-		})
-})
-
-router.get('/ingredients/:id/edit', (req, res) => {
-	Ingredient.findOne({ _id: req.params.id })
-		.then(ingredient => {
-			res.render('editIngredient', {ingredient: ingredient});
-		})
-});
-
-router.post('/ingredients/:id/edit', (req, res) => {
-	console.log('reqy.body:', req.body);
-	Ingredient.findOneAndUpdate({ _id: req.params.id }, req.body, {
-		new: true // returns new ingredient
-	})
-		.then(ingredient => {
-			res.redirect('/')
-		})
-})
+router.post('/', ingredientController.getNewingredients);
+router.get('/ingredients/:id/edit', ingredientController.getEditingredients);
+router.post('/ingredients/:id/edit', ingredientController.postEditingredients);
 
 module.exports = router;
-
-
-
-
-
-
-
-
