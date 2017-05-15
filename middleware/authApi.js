df78920fef1fa38c5237463943b39de2124476eb
鈴木貴大 <TakahiroSuzuki@suzukikidai-no-MacBook-Air.local>
2017-05-15 12:30:46 +1000

    jsonwebtoken

--- middleware/authApi.js
+++ middleware/authApi.js
@@ -11,7 +11,7 @@ const authApi = (req, res, next) => {
   var decoded = jwt.verify(key, 'secretcode');
   console.log(decoded.email)
   let found = false;
-  if (decoded.email === 'shouhei.yamauchi@live.com') {
+  if (decoded.email === 'takahirosuzuki.m010@gmail.com') {
     found = true;
   }
 
