36cf5bcab1fededd81923d1fe807d1b02377fd2d
鈴木貴大 <TakahiroSuzuki@suzukikdainoair.ait-sydney.net.au>
2017-05-18 14:57:27 +1000

    debug port

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ db.once('open', () => {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-console.log(`listen PORT is ${port}`)
+var portTemp = normalizePort(process.env.PORT || '3000');
+console.log(`listen PORT is ${portTemp}`)
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
